ddad6820173d02e3d2b3eebdb84bef45e241ce28
rubatista <rubatista.dev@gmail.com>
2025-07-14 15:30:15 +0100

    finished list goals

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandler() {
+    if (enteredGoal.trim().length === 0) {
+      return;
+    }
+    setGoals(currentGoals => [
+      ...currentGoals,
+      {text: enteredGoal, key: Math.random().toString()}
+    ]);
+    setEnteredGoal('');
+  }
+
+  function deleteGoalHandler(goalIndex) {
+    setGoals(currentGoals => currentGoals.filter((_, index) => index !== goalIndex));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal' onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+        <FlatList data={goals} renderItem={itemData => {
+          return (
+            <View style={styles.goalItem}>
+              <Text>{itemData.item.text}</Text>
+              <Button title='X' onPress={() => deleteGoalHandler(itemData.index)} color='red'/>
+            </View>
+          );
+        }}
+        keyExtractor={(item, index) => {return item.key}}
+        alwaysBounceVertical={false} />
+
+        {goals.length === 0 && <Text>No goals added yet!</Text>}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  appContainer: {
+    padding: 50,
     backgroundColor: '#fff',
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 6,
+  },
+  goalItem: {
+    padding: 5,
+    backgroundColor: '#f0f0f0',
+    marginVertical: 5,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderColor: '#cccccc',
+    borderWidth: 1,
   },
 });
